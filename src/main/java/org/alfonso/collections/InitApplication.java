60e503c5e800a6b9effed7db17f358e3fed7dbcb
Alfonso <alfonsomolerofarman@gmail.com>
2023-01-15 20:08:57 -0500

    add comment

--- src/main/java/org/alfonso/collections/InitApplication.java
+++ src/main/java/org/alfonso/collections/InitApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
-@SpringBootApplication
+@EnableScheduling //Habilita la capacidad de ejecución de tareas programadas de Spring
+@SpringBootApplication //Define que es una aplicacion con spring boot
 public class InitApplication {
 
     public static void main(String[] args) {
