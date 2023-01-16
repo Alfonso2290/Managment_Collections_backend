673d9d7afee9ccd40466ef71d835bc0c34608ced
Alfonso <alfonsomolerofarman@gmail.com>
2023-01-15 19:37:05 -0500

    refactor pom

--- src/main/java/org/alfonso/collections/controller/UserController.java
+++ src/main/java/org/alfonso/collections/controller/UserController.java
@@ -1,13 +1,15 @@
 package org.alfonso.collections.controller;
 
-
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequestMapping("/user")
 public class UserController {
 
-    @GetMapping("/prueba")
+    @GetMapping(value = "/prueba", produces = MediaType.APPLICATION_JSON_VALUE)
     public String prueba(){
         return "probando";
     }
